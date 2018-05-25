834f4e21af6cab957340dd673b5f354427c32533
Andrea Zucchini García <azga84@gmail.com>
2018-05-25 20:51:11 +0200

    initialization of vuejs app, create layout with Vuetify

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import '../../node_modules/vuetify/src/stylus/app.styl'
+
+export function registerSharedComponents () {
+  Vue.use(Vuetify, {
+    components: {
+      VApp,
+      VNavigationDrawer,
+      VFooter,
+      VList,
+      VBtn,
+      VIcon,
+      VGrid,
+      VToolbar,
+      transitions
+    }
+  })
+}
