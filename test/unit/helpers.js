979a5fd768bc39de1a7f18f0e8a2cd6c0bedada8
Andrea Zucchini García <azga84@gmail.com>
2018-05-25 22:19:26 +0200

    create wrapper vuejs component to improve testing

---
+++ test/unit/helpers.js
@@ -0,0 +1,67 @@
+import { mount, shallow } from '@vue/test-utils'
+import Vue from 'vue'
+
+export function WrapComponent (component) {
+  return new Wrap(component)
+}
+
+class Wrap {
+  constructor (component) {
+    this.component = component
+  }
+
+  mount () {
+    return mount(this.component, this.config())
+  }
+
+  shallow () {
+    return shallow(this.component, this.config())
+  }
+
+  withProps (props) {
+    this.props = props
+    return this
+  }
+
+  withStubs (stubs) {
+    this.stubs = stubs
+    return this
+  }
+
+  withSlots (slots) {
+    this.slots = slots
+    return this
+  }
+
+  withRouter (router) {
+    this.router = router
+    return this
+  }
+
+  withStore (store) {
+    this.store = store
+    return this
+  }
+
+  config () {
+    return {
+      propsData: this.props,
+      stubs: this.stubs,
+      slots: this.slots,
+      router: this.router,
+      store: this.store
+    }
+  }
+}
+
+export function waitNextTick () {
+  return Vue.nextTick()
+}
+
+export function waitSetTimeout (delay = 250) {
+  return new Promise(resolve => setTimeout(resolve, delay))
+}
+
+export function stubNow (isoDate) {
+  Date.now = jest.fn(() => new Date(isoDate).valueOf())
+}
