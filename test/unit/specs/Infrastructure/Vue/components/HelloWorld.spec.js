8bae1f78a53726f152e884d1f65ed58c6c5a5bfe
Andrea Zucchini García <azga84@gmail.com>
2018-05-25 22:21:22 +0200

    testing hello world component

---
+++ test/unit/specs/Infrastructure/Vue/components/HelloWorld.spec.js
@@ -0,0 +1,30 @@
+import HelloWorld from '@Vue/components/HelloWorld'
+import { ArtistRepository } from '@/Artist/Domain/ArtistRepository'
+import { WrapComponent } from '../../../../helpers'
+import HelloWorldPageObject from './HelloWorldPageObject'
+
+jest.mock('@/Artist/Domain/ArtistRepository', () => {
+  return {
+    ArtistRepository: {
+      getTopArtistsByCountry: jest.fn(() => Promise.resolve([{
+        name: 'fake artist'
+      }]))
+    }
+  }
+})
+describe('HelloWorld.vue', () => {
+  let wrapper
+  /** @type HelloWorldPageObject */
+  let page
+  beforeEach(() => {
+    wrapper = WrapComponent(HelloWorld).mount()
+    page = new HelloWorldPageObject(wrapper)
+  })
+  it('should render correct contents', () => {
+    const listOfArtist = page.getListOfArtist()
+    expect(page.getBlockQuoteTextContent()).toEqual('First, solve the problem. Then, write the code.')
+    expect(listOfArtist.length).toEqual(1)
+    expect([].slice.call(listOfArtist).pop().textContent).toContain('fake artist')
+    expect(ArtistRepository.getTopArtistsByCountry).toHaveBeenCalled()
+  })
+})
