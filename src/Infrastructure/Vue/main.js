a9f6c23788a2be6df2545d74fd6c8bcd329cf7d8
Andrea Zucchini García <azga84@gmail.com>
2018-05-26 19:23:50 +0200

    call register shared component method

--- src/Infrastructure/Vue/main.js
+++ src/Infrastructure/Vue/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
-import { registerSharedComponents } from './components/index'
-registerSharedComponents()
+import { registerSharedComponents } from './components'
+
 Vue.config.productionTip = false
+
+registerSharedComponents()
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
