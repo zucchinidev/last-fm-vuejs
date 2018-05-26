425732adba9c90f3804ac602e9186df85ba2afbf
Andrea Zucchini García <azga84@gmail.com>
2018-05-26 19:24:20 +0200

    Create ArtistListItem component

---
+++ src/Infrastructure/Vue/components/ArtistListItem/ArtistListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <h2>
+      <a :href="artist.url" target="_blank">
+        {{artist.name}}
+      </a>
+      <img :src="getImage()" alt="artist image">
+    </h2>
+  </li>
+</template>
+<script>
+export default {
+  name: 'ArtistListItem',
+  props: {
+    /** @type Artist */
+    artist: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getImage () {
+      // TODO set not found image url
+      let url = ''
+      if (Object.keys(this.artist).length) {
+        const image = this.artist.images.find(image => image.size === 'medium')
+        if (image) {
+          url = image.url
+        }
+      }
+      return url
+    }
+  }
+}
+</script>
+<style scoped lang="stylus">
+  li
+    display block
+    margin 10px 0
+</style>
