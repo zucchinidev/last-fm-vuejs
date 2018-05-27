de352f2a45f68ee7b05dc2538d3131bb4002b4cb
Andrea Zucchini García <azga84@gmail.com>
2018-05-27 21:58:55 +0200

    create Search Component

---
+++ src/Infrastructure/Vue/components/Search/Search.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-text-field
+    :label="label"
+    :value="model"
+    single-line
+    full-width
+    @input="setValueOfInput"
+    color="teal"
+    prepend-icon="search"
+    dark
+    clearable
+    v-bind="$attr"
+  ></v-text-field>
+</template>
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+      model: this.value
+    }
+  },
+  props: {
+    label: String,
+    trim: {
+      type: Boolean,
+      default: true
+    },
+    value: String
+  },
+  methods: {
+    setValueOfInput (event) {
+      let value = event && event.target ? event.target.value : event
+      if (this.trim && value) {
+        value = value.trim()
+      }
+      this.model = value
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+<style scoped lang="stylus">
+  li
+    display block
+    margin 10px 0
+</style>
