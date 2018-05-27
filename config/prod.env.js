385cc7b50553c8e36cf28c11423ceae5e269cbb4
Andrea Zucchini García <azga84@gmail.com>
2018-05-27 14:35:46 +0200

    include api music base url environment variable in testing and production environment

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_MUSIC_BASE_URL: '"PUT YOUR API MUSIC BASE URL HERE"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  API_MUSIC_BASE_URL: '"PUT YOUR API MUSIC BASE URL HERE"'
 })
