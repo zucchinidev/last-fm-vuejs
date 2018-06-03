2f74f8b72756220c547783e9d9c26d451e375d46
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 19:21:56 +0200

    create left aside component

---
+++ src/Infrastructure/Vue/components/LeftAside/LeftAside.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-navigation-drawer
+    persistent
+    :mini-variant="miniVariant"
+    clipped
+    v-model="drawer"
+    enable-resize-watcher
+    fixed
+    app
+  >
+    <main-menu :items="items"></main-menu>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'LeftAside',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+    miniVariant: Boolean,
+    drawer: Boolean
+  }
+}
+</script>
