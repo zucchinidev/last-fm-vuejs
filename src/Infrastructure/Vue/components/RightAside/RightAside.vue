c05be172a36fabf06089b042607b16f4edf7fabd
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 19:22:20 +0200

    Create Right aside component

---
+++ src/Infrastructure/Vue/components/RightAside/RightAside.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-navigation-drawer
+    temporary
+    right
+    v-model="model"
+    fixed
+    app
+    @input="onChangeNavigation"
+  >
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>compare_arrows</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'RightAside',
+  props: {
+    rightDrawer: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      model: this.rightDrawer
+    }
+  },
+  watch: {
+    rightDrawer (val) {
+      this.model = val
+    }
+  },
+  methods: {
+    onChangeNavigation () {
+      this.$emit('change', this.model)
+    }
+  }
+}
+</script>
