526143901fdd0eecbb0ea8ff74849a5c27e9db6d
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 19:22:43 +0200

    Export new components

--- src/Infrastructure/Vue/components/index.js
+++ src/Infrastructure/Vue/components/index.js
@@ -18,6 +18,11 @@ import ArtistListItem from './ArtistListItem/ArtistListItem'
 import TrackCardItem from './TrackCardItem/TrackCardItem'
 import Search from './Search/Search'
 import Player from './Player/Player'
+import MFooter from './Footer/Footer'
+import RightAside from './RightAside/RightAside'
+import MainMenu from './MainMenu/MainMenu'
+import LeftAside from './LeftAside/LeftAside'
+import Toolbar from './Toolbar/Toolbar'
 
 export function registerSharedComponents () {
   Vue.use(Vuetify, {
@@ -38,5 +43,10 @@ export function registerSharedComponents () {
   Vue.component('ArtistListItem', ArtistListItem)
   Vue.component('TrackCardItem', TrackCardItem)
   Vue.component('Search', Search)
+  Vue.component('Toolbar', Toolbar)
+  Vue.component('LeftAside', LeftAside)
+  Vue.component('MainMenu', MainMenu)
+  Vue.component('RightAside', RightAside)
+  Vue.component('MFooter', MFooter)
   Vue.component('Player', Player)
 }
