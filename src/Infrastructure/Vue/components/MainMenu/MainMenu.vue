78180d1802485ef599d5aa7ea72f60452a4e8b2c
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 19:22:07 +0200

    create main menu component

---
+++ src/Infrastructure/Vue/components/MainMenu/MainMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list>
+    <v-list-tile
+      v-for="(item, index) in items"
+      :key="index" @click="">
+
+      <v-list-tile-action>
+        <v-icon v-html="item.icon"></v-icon>
+      </v-list-tile-action>
+
+      <v-list-tile-content>
+        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+      </v-list-tile-content>
+
+    </v-list-tile>
+
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'MainMenu',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
