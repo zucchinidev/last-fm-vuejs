34732c99f7c680809ab6a33b8dddbb27b674edb7
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 18:30:22 +0200

    Install bus plugin in the setup moment

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import { registerSharedComponents } from '../../src/Infrastructure/Vue/components'
 import Router from 'vue-router'
+import { Bus } from '@Vue/plugins/bus'
+
 registerSharedComponents()
 
+Vue.use(Bus)
 Vue.use(Router)
 Vue.config.productionTip = false
