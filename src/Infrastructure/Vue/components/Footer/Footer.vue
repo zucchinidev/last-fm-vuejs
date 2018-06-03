6d42ff318a32b9a5f2c02faaeba112134a4d3274
Andrea Zucchini García <azga84@gmail.com>
2018-06-03 19:21:47 +0200

    create footer component

---
+++ src/Infrastructure/Vue/components/Footer/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer :fixed="fixed" app>
+    <span>&copy; 2018</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: ['fixed']
+}
+</script>
+<style scoped>
+  footer span{
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
