62e45501da0f0be3e02f870c3ac75724798c92a8
Andrea Zucchini García <azga84@gmail.com>
2018-06-04 23:16:37 +0200

    if Card Item component work in extended mode, we show the big image

--- src/Infrastructure/Vue/components/TrackCardItem/TrackCardItem.vue
+++ src/Infrastructure/Vue/components/TrackCardItem/TrackCardItem.vue
@@ -1,12 +1,35 @@
 <template>
-  <v-flex xs12 sm6 md3 lg3 wrap>
+  <v-flex v-bind="breakPoints" wrap v-if="track && track.album">
     <v-card class="white--text">
       <v-container fluid grid-list-lg>
         <v-layout row>
           <v-flex xs7>
-            <div>
-              <div class="headline">{{track.album.name}}</div>
-              <div>{{track.name}}</div>
+            <div class="text-xs-center">
+              <template v-if="!extendedData">
+                <div class="headline">{{track.album.name}}</div>
+                <div>{{track.name}}</div>
+              </template>
+              <template v-if="extendedData">
+                <p class="headline">Album <v-icon>arrow_right_alt</v-icon>{{track.album.name}}</p>
+                <p>Track name <v-icon>arrow_right_alt</v-icon> {{track.name}}</p>
+                <p>Release <v-icon>arrow_right_alt</v-icon> {{track.album.release}}</p>
+                <p> Popularity <v-icon>arrow_right_alt</v-icon> {{track.popularity}}</p>
+                <div>
+                  <p class="display-1 text-xs-left">
+                    Artists <v-icon>trending_down</v-icon>
+                  </p>
+                  <ul>
+                    <li
+                      class="text-xs-center"
+                      :key="index"
+                      v-for="(artist, index) in track.album.artists">
+                      <v-chip>
+                        {{artist}}
+                      </v-chip>
+                    </li>
+                  </ul>
+                </div>
+              </template>
             </div>
           </v-flex>
           <v-flex xs5>
@@ -15,7 +38,7 @@
               height="125px"
               contain
             ></v-card-media>
-            <v-card-actions>
+            <v-card-actions v-if="!extendedData">
               <v-spacer></v-spacer>
               <v-btn icon @click="selectTrack">
                 <v-icon>play_arrow</v-icon>
@@ -36,18 +59,38 @@ export default {
     track: {
       type: Object,
       required: true
+    },
+    breakPointsWrapper: {
+      type: Object,
+      default: () => ({})
+    },
+    extendedData: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
     imageSrc () {
       let url = ''
       if (Object.keys(this.track).length) {
-        const image = this.track.getSmallImage()
+        const image = this.extendedData ? this.track.getBigImage() : this.track.getSmallImage()
         if (image) {
           url = image.url
         }
       }
       return url
+    },
+    breakPoints () {
+      const defaultBreakPoints = {
+        'xs12': true,
+        'sm6': true,
+        'md3': true,
+        'lg3': true
+      }
+      return {
+        ...defaultBreakPoints,
+        ...this.breakPointsWrapper
+      }
     }
   },
   methods: {
@@ -57,6 +100,9 @@ export default {
   }
 }
 </script>
-<style scoped lang="stylus">
-
+<style lang="stylus">
+  ul
+    display flex
+    list-style none
+    margin-bottom 1rem
 </style>
