f0fba408e5902e74e52f61490442787dcb077f5e
Andrea Zucchini García <azga84@gmail.com>
2018-06-05 22:53:32 +0200

    install filter in test environment

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import { registerSharedComponents } from '../../src/Infrastructure/Vue/components'
 import Router from 'vue-router'
 import { Bus } from '@Vue/plugins/bus'
+import { millisecondsToMinutes } from '../../src/Infrastructure/Vue/filters/milliseconds-to-minutes'
 
 registerSharedComponents()
 
 Vue.use(Bus)
 Vue.use(Router)
+Vue.use(millisecondsToMinutes)
 Vue.config.productionTip = false
