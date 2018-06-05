de71d90155a7298dbb774a19f69e9ae0e4519dbb
Andrea Zucchini García <azga84@gmail.com>
2018-06-05 20:06:19 +0200

    Install milliseconds to minutes filter

--- src/Infrastructure/Vue/main.js
+++ src/Infrastructure/Vue/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import router from './router/index'
 import { registerSharedComponents } from './components'
 import { Bus } from './plugins/bus'
+import { millisecondsToMinutes } from './filters/milliseconds-to-minutes'
 
 Vue.use(Bus)
 Vue.config.productionTip = false
+Vue.use(millisecondsToMinutes)
 registerSharedComponents()
 /* eslint-disable no-new */
 new Vue({
