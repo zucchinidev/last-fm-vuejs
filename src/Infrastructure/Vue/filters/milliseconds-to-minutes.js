5e7098cde99f82f792b200be56e59a7ec63284c5
Andrea Zucchini García <azga84@gmail.com>
2018-06-05 22:52:15 +0200

    export main function to improve testing

--- src/Infrastructure/Vue/filters/milliseconds-to-minutes.js
+++ src/Infrastructure/Vue/filters/milliseconds-to-minutes.js
@@ -1,12 +1,13 @@
+export const calculateResult = (originalValue) => {
+  const milliseconds = Number(originalValue)
+  const minutes = Math.floor(milliseconds / 60000)
+  const seconds = (minutes % 60000 / 1000).toFixed(0)
+  let result = minutes < 10 ? `0${minutes}:` : `${minutes}:`
+  result += seconds < 10 ? `0${seconds}` : seconds
+  return result
+}
 export const millisecondsToMinutes = {
   install (Vue) {
-    Vue.filter('ms-to-mm', (originalValue) => {
-      const milliseconds = Number(originalValue)
-      const minutes = Math.floor(milliseconds / 60000)
-      const seconds = (minutes % 60000 / 1000).toFixed(0)
-      let result = minutes < 10 ? `0${minutes}:` : `${minutes}:`
-      result += seconds < 10 ? `0${seconds}` : seconds
-      return result
-    })
+    Vue.filter('ms-to-mm', calculateResult)
   }
 }
