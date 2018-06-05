e8ff5146215c1992e13e94642abd502735ea5358
Andrea Zucchini García <azga84@gmail.com>
2018-06-05 20:06:00 +0200

    Create milliseconds to minutes filter

---
+++ src/Infrastructure/Vue/filters/milliseconds-to-minutes.js
@@ -0,0 +1,12 @@
+export const millisecondsToMinutes = {
+  install (Vue) {
+    Vue.filter('ms-to-mm', (originalValue) => {
+      const milliseconds = Number(originalValue)
+      const minutes = Math.floor(milliseconds / 60000)
+      const seconds = (minutes % 60000 / 1000).toFixed(0)
+      let result = minutes < 10 ? `0${minutes}:` : `${minutes}:`
+      result += seconds < 10 ? `0${seconds}` : seconds
+      return result
+    })
+  }
+}
