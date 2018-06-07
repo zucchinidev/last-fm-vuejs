b4de2092d09891dc1476db7f44cbecf340eeddd2
Andrea Zucchini García <azga84@gmail.com>
2018-06-07 18:18:12 +0200

    create preview url empty page object to improve access dom in testing

---
+++ test/unit/specs/Infrastructure/Vue/directives/PreviewUrlEmptyPageObject.js
@@ -0,0 +1,22 @@
+export class PreviewUrlEmptyPageObject {
+  constructor (vm) {
+    this.vm = vm
+  }
+
+  isFilterApplied () {
+    return this.vm.$el.querySelector('.card__media').style.filter === 'blur(3px)'
+  }
+
+  hasCursorNotAllowed () {
+    return this.vm.$el.querySelector('.card__actions').style.cursor === 'not-allowed'
+  }
+
+  hasPlayButtonDisabled () {
+    const button = this.vm.$el.querySelector('button.play-arrow')
+    return button.getAttribute('disabled') === 'disabled' && button.classList.contains('btn--disabled')
+  }
+
+  changeHasPreview (value) {
+    this.vm.hasPreview = value
+  }
+}
