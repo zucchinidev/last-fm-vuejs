ec949e9e058bd4661ae51219123d36e8ea61500f
Andrea Zucchini García <azga84@gmail.com>
2018-06-10 20:01:22 +0200

    improve html of player

--- src/Infrastructure/Vue/components/Player/Player.vue
+++ src/Infrastructure/Vue/components/Player/Player.vue
@@ -1,36 +1,30 @@
 <template>
-  <v-flex xs12 sm6 offset-sm3>
-    <v-card v-if="hasTrack()">
-      <v-container>
-        <v-layout row>
-          <v-flex xs7>
-            <v-card-media
-              :src="selectedTrack.getSmallImage().url"
-              height="125px"
-              contain
-            ></v-card-media>
-          </v-flex>
-          <v-flex xs5>
-            <div>
-              <div class="title">
-                {{selectedTrack.name}}
-              </div>
-              <div>
-                {{selectedTrack.duration | ms-to-mm}}
-              </div>
-            </div>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <audio :src="selectedTrack.getPreviewUrl()" controls></audio>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </v-flex>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs3>
+        <div class="track__data__container" v-if="hasTrack()">
+          <img :src="getTrackImage()" :alt="getTrackAlt()">
+          <div>
+            <p class="title">
+              {{track.album.name}}
+            </p>
+            <p>
+              {{track.name}}
+              <span v-if="hasTrack()">{{track.duration | ms-to-mm}}</span>
+            </p>
+          </div>
+        </div>
+      </v-flex>
+      <v-flex xs9>
+        <div class="audio__container">
+          <audio v-if="hasTrack()" :src="getPreviewUrl()" controls></audio>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
+
 export default {
   name: 'Player',
   props: {
@@ -39,18 +33,54 @@ export default {
       type: Object
     }
   },
+  data () {
+    return {
+      track: {}
+    }
+  },
   methods: {
     hasTrack () {
-      return this.selectedTrack && this.selectedTrack.album
+      return this.track && this.track.album
+    },
+    getTrackImage () {
+      let url = ''
+      const image = this.track.getSmallImage()
+      if (image) {
+        url = image.url
+      }
+      return url
+    },
+    getTrackAlt () {
+      return this.track.name
+    },
+    getPreviewUrl () {
+      return this.track.getPreviewUrl()
     }
   },
   created () {
-    this.$bus.$on('set-track', track => (this.selectedTrack = track))
+    this.$bus.$on('set-track', track => (this.track = track))
+    if (this.selectedTrack) {
+      const Ctrl = Object.getPrototypeOf(this.selectedTrack).constructor
+      this.track = Object.assign(new Ctrl(), this.selectedTrack)
+    }
   }
 }
 </script>
-<style scoped>
-  .card__media {
-    clip-path: circle(25%);
-  }
+<style scoped lang="stylus">
+  .card__media
+    clip-path: circle(25%)
+
+  .audio__container
+    text-align: center
+
+  .track__data__container
+    display flex
+    > img
+      margin-right: 10px
+
+  .container
+    padding-top: 5px
+
+  audio
+    margin-top: 20px
 </style>
