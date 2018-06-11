21d5b1dfb6481b6c842e3f6b77e3be7afbb239f0
Andrea Zucchini García <azga84@gmail.com>
2018-06-11 22:33:58 +0200

    Apply animation when the song is selected. The player show right to left

--- src/Infrastructure/Vue/components/Footer/Footer.vue
+++ src/Infrastructure/Vue/components/Footer/Footer.vue
@@ -1,17 +1,24 @@
 <template>
-  <v-footer :fixed="fixed" height="90" app>
-    <player></player>
-  </v-footer>
+  <transition name="move">
+    <v-footer :fixed="fixed" height="90" v-show="hasTrack" app>
+      <player></player>
+    </v-footer>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'Footer',
-  props: ['fixed']
+  props: ['fixed'],
+  data: () => ({hasTrack: false}),
+  created () {
+    this.$bus.$on('set-track', () => (this.hasTrack = true))
+  }
 }
 </script>
 <style lang="stylus" scoped>
   footer.footer.footer--fixed
     min-width: 620px
     border-top: 1px solid #cbcbcb
+    box-shadow: 0 -2px 18px 3px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)
 </style>
