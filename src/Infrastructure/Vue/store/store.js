bc2d6d25e25697168d8e285208a8d1d7eb1504c0
Andrea Zucchini García <azga84@gmail.com>
2018-06-12 21:43:39 +0200

    create Vuex store

---
+++ src/Infrastructure/Vue/store/store.js
@@ -0,0 +1,12 @@
+import * as mutations from './mutations'
+import * as actions from './actions'
+
+export default {
+  state: {
+    track: {},
+    tracks: []
+  },
+  mutations,
+  actions,
+  getters: {}
+}
